63909d29254050bd3b8e7b8805d4ef8a0c23ab4a
NandoCodeBR <52359881+NandoCodeBR@users.noreply.github.com>
2020-01-19 13:35:07 -0300

    Add files via upload

---
+++ Exercicio.php
@@ -0,0 +1,16 @@
+<?php
+$equipeA = array (0,7,5,2);
+$equipeB = array (6,1,5);
+$x = 0;
+$count = 0;
+while ($x<=3) {
+if ($equipeA[$x]<$equipeB[0]){
+    array $count[$x] =($equipeA[$x]);
+}
+$x++;
+
+}
+echo "O número de partidas que a equipe A marcou a menos que a equipe B é de". $count[0];
+
+ 
+?>
